fb73f471a774f3d26ca7c49d99fe8432d6b12085
awodeyia1928 <awodeyia4449@student.babcock.edu.ng>
2025-09-28 11:33:04 +0100

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,13 @@
+// VARIABLES (let, const, var)
+
+// let → block-scoped, can be reassigned
+let score = 10;
+score = 15;
+
+// const → block-scoped, cannot be reassigned
+const pi = 3.1416;
+
+// var → function-scoped (older, avoid if possible)
+var name = "Ayoola";
+
+console.log(score, pi, name);
