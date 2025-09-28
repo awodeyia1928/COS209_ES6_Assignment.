425fcccc07c1b48770a812ae9ceedb0f2607497b
awodeyia1928 <awodeyia4449@student.babcock.edu.ng>
2025-09-28 11:38:52 +0100

    Create modules.js

---
+++ modules.js
@@ -0,0 +1,11 @@
+// MODULES
+// In ES6, we can export and import code between files.
+
+// ----- mathUtils.js -----
+// export const add = (a, b) => a + b;
+// export const multiply = (a, b) => a * b;
+
+// ----- main.js -----
+// import { add, multiply } from './mathUtils.js';
+// console.log(add(2, 3));
+// console.log(multiply(4, 5));
