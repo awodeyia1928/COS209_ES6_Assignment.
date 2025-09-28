0977ff75d962821a1ae9f4c34420bdb5717318b0
awodeyia1928 <awodeyia4449@student.babcock.edu.ng>
2025-09-28 11:39:42 +0100

    Create ternaryOperators.js

---
+++ ternaryOperators.js
@@ -0,0 +1,7 @@
+// TERNARY OPERATOR
+// A shorthand way of writing if-else statements.
+
+const age = 18;
+const canVote = age >= 18 ? "Yes, you can vote." : "No, you cannot vote.";
+
+console.log(canVote);
